ec73889f0a6ba51759d13742bd5e86a1b861eae6
T-ara32 <x04.magic.z5@gmail.com>
2022-07-31 10:36:15 +0900

    event計算

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -9,8 +9,10 @@ use App\Services\EventService;
 class Calendar extends Component
 {
     public $currentDate;
-    public $day;
     public $currentWeek;
+    public $day;
+    public $checkDay;
+    public $dayOfWeek;
     public $sevenDaysLater;
     public $events;
 
@@ -27,7 +29,14 @@ class Calendar extends Component
 
         for($i = 0; $i < 7; $i++) {
             $this->day = CarbonImmutable::today()->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = CarbonImmutable::today()->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::today()->addDays($i)->dayName;
+            array_push($this->currentWeek, [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek,
+            ]);
+            // dd($this->currentWeek);
         }
     }
 
@@ -42,9 +51,15 @@ class Calendar extends Component
             $this->sevenDaysLater->format('Y-m-d'),
         );
 
-        for($i = 0; $i < 7; $i++) {
+        for($i = 0; $i < 7 ; $i++){
             $this->day = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::parse($this->currentDate)->addDays($i)->dayName;
+            array_push($this->currentWeek, [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek
+            ]);
         }
     }
 
--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -2,7 +2,6 @@
 
 namespace App\Services;
 
-
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
@@ -10,46 +9,42 @@ class EventService
 {
   public static function checkEventDuplication($eventDate, $startTime, $endTime)
   {
-    $check = DB::table('events')
-    ->whereDate('start_date', $eventDate)
-    ->whereTime('end_date', '>', $startTime)
-    ->whereTime('start_date', '<', $endTime)
-    ->exists();
+    return DB::table('events')
+        ->whereDate('start_date', $eventDate)
+        ->whereTime('end_date', '>', $startTime)
+        ->whereTime('start_date', '<', $endTime)
+        ->exists();
+   }
 
-    return $check;
-  }
+   public static function countEventDuplication($eventDate, $startTime, $endTime)
+   {
+     return DB::table('events')
+         ->whereDate('start_date', $eventDate)
+         ->whereTime('end_date', '>', $startTime)
+         ->whereTime('start_date', '<', $endTime)
+         ->count();
+    }
 
   public static function joinDateAndTime($date, $time)
   {
-    $join = $date . " " . $time;
-    return Carbon::createFromFormat('Y-m-d H:i' , $join);
-  }
-
-  public static function countEventDuplication($eventDate, $startTime, $endTime)
-  {
-    $check = DB::table('events')
-    ->whereDate('start_date', $eventDate)
-    ->whereTime('end_date', '>', $startTime)
-    ->whereTime('start_date', '<', $endTime)
-    ->count();
-
-    return $check;
+    $join = $date. " " .  $time;
+    return Carbon::createFromFormat('Y-m-d H:i', $join);
   }
 
   public static function getWeekEvents($startDate, $endDate)
   {
     $reservedPeople = DB::table('reservations')
-    ->select('event_id',
-    DB::raw('sum(number_of_people) as number_of_people'))
-    ->whereNull('canceled_date')
-    ->groupBy('event_id');
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        ->whereNotNull('canceled_date')
+        ->groupBy('event_id');
 
-    return DB::table('events')
-    ->leftJoinSub($reservedPeople, 'reservedPeople', function($join) {
-        $join->on('events.id', '=', 'reservedPeople.event_id');
-    })
-    ->whereBetween('start_date', [$startDate, $endDate])
-    ->orderBy('start_date', 'desc')
-    ->get();
+        return DB::table('events')
+        ->leftJoinSub($reservedPeople, 'reservedPeople', function($join){
+            $join->on('events.id', '=', 'reservedPeople.event_id');
+            })
+        ->whereBetween('start_date', [$startDate, $endDate])
+        ->orderBy('start_date', 'asc')
+        ->get();
   }
+
 }
